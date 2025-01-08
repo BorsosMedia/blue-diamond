9147838e25a1ee046c9dbf62aebf1b7eb093d622
BorsosMedia <support@borsosmedia.com>
2025-01-08 15:53:39 -0400

    Update copyright year in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -226,7 +226,7 @@ function Footer({ HandleMenuSection, setHandleMenuSection }) {
       </div>
       <div className="txt-ftr-ctnr">
         <p className="ftr-txt">
-          © Copyright 2023 - All Rights Reserved.
+          © Copyright 2024 - All Rights Reserved.
           <br /> Website designed and developed by
           <a href="https://www.borsosmedia.com/" target="_blank">
             Borsos Media
